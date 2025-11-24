033fb465922f9f9bad02ccebb7ee3f67483414d9
bahabelom <bahabelomgebremedhn2@gmail.com>
2025-11-24 16:43:15 +0300

    use configration instead of hardcoded

--- src/kafka/kafka.service.ts
+++ src/kafka/kafka.service.ts
@@ -1,15 +1,24 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { Kafka, Consumer, Producer, EachMessagePayload } from 'kafkajs';
+import { configuration } from '../config/configuration';
 
 @Injectable()
 export class KafkaService implements OnModuleInit, OnModuleDestroy {
+  private readonly config = configuration();
   private kafka = new Kafka({
-    clientId: 'my-app',
-    brokers: ['localhost:9092'], // make sure Kafka is running here
+    clientId: this.config.kafka.clientId,
+    brokers: this.config.kafka.brokers,
   });
 
-  private producer: Producer = this.kafka.producer();
-  private consumer: Consumer = this.kafka.consumer({ groupId: 'my-group' });
+  private producer: Producer = this.kafka.producer({
+    maxInFlightRequests: this.config.kafka.producer.maxInFlightRequests,
+    idempotent: this.config.kafka.producer.idempotent,
+  });
+  private consumer: Consumer = this.kafka.consumer({ 
+    groupId: this.config.kafka.groupId,
+    sessionTimeout: this.config.kafka.consumer.sessionTimeout,
+    heartbeatInterval: this.config.kafka.consumer.heartbeatInterval,
+  });
 
   async onModuleInit() {
     // Connect producer and consumer
@@ -17,7 +26,7 @@ export class KafkaService implements OnModuleInit, OnModuleDestroy {
     await this.consumer.connect();
 
     // Subscribe to topic
-    await this.consumer.subscribe({ topic: 'test-topic', fromBeginning: true });
+    await this.consumer.subscribe({ topic: this.config.kafka.topics.example, fromBeginning: true });
 
     // Listen for messages
     await this.consumer.run({
@@ -31,7 +40,7 @@ export class KafkaService implements OnModuleInit, OnModuleDestroy {
 
   async sendMessage(message: string) {
     await this.producer.send({
-      topic: 'test-topic',
+      topic: this.config.kafka.topics.example,
       messages: [{ value: message }],
     });
     console.log(`Sent message: ${message}`);
